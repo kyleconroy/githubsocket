68aa687977f02026bcb9380159a1f204c33e38f0
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-10-02 11:36:23 -0700

    Use command line flags instead of a config file

--- README.md
+++ README.md
@@ -8,18 +8,17 @@ You'll need Go and the `websocket` module.
 
     $ go get code.google.com/p/go.net/websocket
 
-`config.json` contains your Github API credentials
+The server looks for the following environment varaibles
 
-```json
-{
-  "username": "GITHUB_USERNAME",
-  "password": "GITHUB_PASSWORD"
+```bash
+$ export GITHUB_USERNAME=""
+$ export GITHUB_PASSWORD=""
 }
 ```
 
 ## Running
 
-    $ go run server.go config.json
+    $ go run server.go
      * http://localhost:23456
 
 You are now ready for Github hooks in your browser.
--- server.go
+++ server.go
@@ -1,94 +1,98 @@
 package main
 
 import (
-		"flag"
-		"fmt"
-		"net/http"
-		"code.google.com/p/go.net/websocket"
-		"math/rand"
-		"strconv"
-		"time"
+	"code.google.com/p/go.net/websocket"
+	"flag"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
 )
 
-
 var port *int = flag.Int("p", 23456, "Port to listen.")
-var dispatch map[int64] chan int = make(map[int64] chan int)
+var user *string = flag.String("user", "", "Github username.")
+var pass *string = flag.String("pass", "", "Github password.")
+
+var dispatch map[int64]chan int = make(map[int64]chan int)
 
 type Sub struct {
-		Mode string
-		Topic string
+	Mode  string
+	Topic string
 }
 
 // jsonServer echoes back json string sent from client using websocket.JSON.
 func hubServer(ws *websocket.Conn) {
-		id := rand.Int63()
+	id := rand.Int63()
 
-		dispatch[id] = make(chan int)
-		defer delete(dispatch, id)
+	dispatch[id] = make(chan int)
+	defer delete(dispatch, id)
 
-		fmt.Printf("listern %d\n", id)
+	fmt.Printf("listern %d\n", id)
 
-		go func() {
-				for {
-				// Send send a text message serialized T as JSON.
-				event := <- dispatch[id]
-
-				err := websocket.JSON.Send(ws, event)
-
-				if err != nil {
-						fmt.Println(err)
-						break
-				}
-
-				fmt.Print(".")
-				}
-		}()
-
+	go func() {
 		for {
-				var msg Sub
+			// Send send a text message serialized T as JSON.
+			event := <-dispatch[id]
 
-				// Receive receives a text message serialized T as JSON.
-				err := websocket.JSON.Receive(ws, &msg)
+			err := websocket.JSON.Send(ws, event)
 
-				if err != nil {
-						fmt.Println(err)
-						break
-				}
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
 
+			fmt.Print(".")
+		}
+	}()
 
-				fmt.Printf("callback: http://incoming?socket=%d\n", id)
+	for {
+		var msg Sub
+
+		// Receive receives a text message serialized T as JSON.
+		err := websocket.JSON.Receive(ws, &msg)
+
+		if err != nil {
+			fmt.Println(err)
+			break
 		}
 
-		fmt.Printf("close")
+		fmt.Printf("callback: http://incoming?socket=%d\n", id)
+	}
+
+	fmt.Printf("close")
 }
 
 func HookListener(w http.ResponseWriter, req *http.Request) {
-		socket := req.URL.Query().Get("socket")
-		id, err := strconv.ParseInt(socket, 10, 64)
+	socket := req.URL.Query().Get("socket")
+	id, err := strconv.ParseInt(socket, 10, 64)
 
-		if err != nil {
-				fmt.Fprint(w, "Bad socket id")
-				return
-		}
+	if err != nil {
+		fmt.Fprint(w, "Bad socket id")
+		return
+	}
 
-		go func() {
-				dispatch[id] <- 45
-		}()
+	go func() {
+		dispatch[id] <- 45
+	}()
 
-		fmt.Fprintf(w, "Socket %s contacted", socket)
+	fmt.Fprintf(w, "Socket %s contacted", socket)
 }
 
 func main() {
-		rand.Seed( time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().UTC().UnixNano())
 
-		flag.Parse()
-		http.Handle("/hub", websocket.Handler(hubServer))
-		http.HandleFunc("/incoming", HookListener)
-		http.Handle("/", http.FileServer(http.Dir("")))
+	flag.Parse()
 
-		fmt.Printf(" * http://localhost:%d/\n", *port)
-		err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
-		if err != nil {
-				panic("ListenANdServe: " + err.Error())
-		}
+	http.Handle("/hub", websocket.Handler(hubServer))
+	http.Handle("/", http.FileServer(http.Dir("")))
+	http.HandleFunc("/incoming", HookListener)
+
+	fmt.Printf(" * http://localhost:%d/\n", *port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+
+	if err != nil {
+		panic("ListenANdServe: " + err.Error())
+	}
 }
